bd3dc1fbdd533c2ce63a45d750410be9ad8ffd72
cabarbaye <cabarbaye.adrien@gmail.com>
2024-07-10 15:37:56 +0200

    test ok

--- Minishell/srcs/exec/00_error.c
+++ Minishell/srcs/exec/00_error.c
@@ -3,52 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yonieva <yonieva@student.42perpignan.fr    +#+  +:+       +#+        */
+/*   By: acabarba <acabarba@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/10 14:56:19 by acabarba          #+#    #+#             */
-/*   Updated: 2024/07/10 15:27:47 by yonieva          ###   ########.fr       */
+/*   Created: 2024/07/10 15:37:47 by acabarba          #+#    #+#             */
+/*   Updated: 2024/07/10 15:37:48 by acabarba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
-
-void    set_bool(t_mutex *mutex, bool *dest, bool value)
-{
-    safe_mutex(mutex, LOCK);
-    *dest = value;
-    safe_mutex(mutex, UNLOCK);
-}
-
-bool    get_bool(t_mutex *mutex, bool *value)
-{
-    bool ret;
-
-    safe_mutex(mutex, LOCK);
-    ret = *value;
-    safe_mutex(mutex, UNLOCK);
-    return (ret);
-}
-
-long    get_long(t_mutex *mutex, long *value)
-{
-    long    ret;
-
-    safe_mutex(mutex, LOCK);
-    ret = *value;
-    safe_mutex(mutex, UNLOCK);
-    return (ret);
-
-
-}
-
-void    set_long(t_mutex *mutex, long *dest, long value)
-{
-    safe_mutex(mutex, LOCK);
-    *dest = value;
-    safe_mutex(mutex, UNLOCK);
-}
-
-bool    diner_finished(t_data *table)
-{
-    return (get_bool(&table->table_mutex, &table->end));
-}
