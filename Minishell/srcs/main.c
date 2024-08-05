204ac8b26749163cccd64379c1919d7161e0e28b
cabarbaye <cabarbaye.adrien@gmail.com>
2024-08-05 15:08:29 +0200

    maj main

--- Minishell/srcs/main.c
+++ Minishell/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: acabarba <acabarba@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 14:39:22 by acabarba          #+#    #+#             */
-/*   Updated: 2024/08/05 14:54:12 by acabarba         ###   ########.fr       */
+/*   Updated: 2024/08/05 15:08:21 by acabarba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,27 @@ int	main(int ac, char **av)
 	t_shell_level *shlvl;
 	
 		// // 1 - Création de l'environnement.
-		// envp_init()
+			// envp_init()
+
+
+			
+			
 		// // 2 - Initialisation de l'environnement
-		// shlvl = init_shlvl();
-		// while (1)
-		// {
-		// // 3 - Environnement créé / While en boucle en attente de commande
-		// signature();
+			// shlvl = init_shlvl();
+	// signature(); //
+	
+
+
+	
+		// // 3 - Interface créé avec les données de L'environement / While en boucle en attente de commande
 		// // 3 - Parsing de la commande saisie par l'utilisateur (av de l'env)
-		token_list = main_parse(ac, av);
-		// getchar();
+			// while (1)
+			// {
+			token_list = main_parse(ac, av); // potientiellement changer ac, av
+
+
+
+
 
 		// // 4 - Exécution de la commande saisie par l'utilisateur
 			// // selection builtin ou non 
@@ -36,9 +47,13 @@ int	main(int ac, char **av)
 			// //				selection du builtin
 			// // si non :
 			// //		go EXECV
+			
 		// free token list
+
+
+		
 		// // 5 - Rendre la main à l'utilisateur pour prochaine commande
-		}
+		// }
 	
 
 	// test 
