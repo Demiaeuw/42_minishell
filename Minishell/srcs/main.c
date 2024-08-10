b2ecd878592c879b1723fdb38d7c4cb818955d90
Cabarbaye <cabarbaye.adrien@gmail.com>
2024-08-10 23:09:41 +0200

    leaks de readline et de history
    Co-authored-by: Gauthier Esteve <gaesteve42@users.noreply.github.com>

--- Minishell/srcs/main.c
+++ Minishell/srcs/main.c
@@ -6,11 +6,26 @@
 /*   By: acabarba <acabarba@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 14:39:22 by acabarba          #+#    #+#             */
-/*   Updated: 2024/08/10 22:30:45 by acabarba         ###   ########.fr       */
+/*   Updated: 2024/08/10 23:09:08 by acabarba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+//debut de test
+static char	*read_input(void)
+{
+	char *buffer = (char*)malloc(sizeof(char) * BUFFER_SIZE);
+	ssize_t bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1);
+	if (bytes_read < 0)
+	{
+		free(buffer);
+		return NULL;
+	}
+	buffer[bytes_read] = '\0';
+	return (buffer);
+}
+//fin de test
 
 int	main(int ac, char **av, char **env)
 {
@@ -27,17 +42,18 @@ int	main(int ac, char **av, char **env)
 
 	while (1)
 	{
-		input = readline("minishell> ");
+		// input = readline("minishell> ");
+		input = read_input();
 		if (input == NULL)
 			break ;
 		if (*input)
-			add_history(input);
+			// add_history(input);
 		token_list = main_parse(input);
 		free(input);
 		main_exec(token_list, env_list);
 		free_token_list(&token_list, free_token_value);
 	}
 	free_env_list(env_list);
-	clear_history();
+	// clear_history();
 	return (0);
 }
