aba294560bf00024dec9fb30c4d6eab7e0dd9410
CABARBAYE Adrien <cabarbaye.adrien@gmail.com>
2024-09-26 15:08:43 +0200

    cd leak ok et fonctionne

--- minishell/srcs/builtin/00_mini_cd.c
+++ minishell/srcs/builtin/00_mini_cd.c
@@ -21,6 +21,7 @@ int	exe_cd(char *input, t_envp *envp)
 	char	*new_pwd;
 	char	cwd[BUFFER_SIZE];
 	char	*path;
+	char	*home_path = NULL;  // To keep track of HOME path if dynamically allocated
 
 	if (ft_strncmp(input, "cd", 2) != 0)
 		return (-1);
@@ -28,17 +29,33 @@ int	exe_cd(char *input, t_envp *envp)
 	while (*path == ' ')
 		path++;
 	if (*path == '\0' || ft_strcmp(path, "~") == 0)
-		path = get_env_value("HOME", envp->env);
+	{
+		home_path = get_env_value("HOME", envp->env);
+		if (!home_path)
+			return (printf("cd: HOME not set\n"), 1);
+		path = home_path;  // Assign dynamically allocated HOME path
+	}
 	old_pwd = getcwd(cwd, sizeof(cwd));
 	if (!old_pwd)
 		return (error_flag(), 1);
 	if (chdir(path) != 0)
+	{
+		if (home_path)
+			free(home_path);  // Free dynamically allocated HOME path
 		return (printf("cd: no such file or directory: %s\n", path), 1);
+	}
 	new_pwd = getcwd(cwd, sizeof(cwd));
 	if (!new_pwd)
+	{
+		if (home_path)
+			free(home_path);  // Free dynamically allocated HOME path
 		return (error_flag(), 1);
+	}
 	set_env_value(envp->env, "OLDPWD", old_pwd);
 	set_env_value(envp->env, "PWD", new_pwd);
-	free(path);
+
+	if (home_path)
+		free(home_path);  // Free dynamically allocated HOME path after use
 	return (0);
 }
+
