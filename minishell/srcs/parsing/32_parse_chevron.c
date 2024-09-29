910fef28f4890c23f7d75e48b6f65b7b5f626921
Yoan <onievayoan@gmail.com>
2024-09-29 22:38:59 +0200

    valeur pointe after chevron

--- minishell/srcs/parsing/32_parse_chevron.c
+++ minishell/srcs/parsing/32_parse_chevron.c
@@ -6,7 +6,7 @@
 /*   By: yonieva <yonieva@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 16:08:51 by acabarba          #+#    #+#             */
-/*   Updated: 2024/09/29 22:32:56 by yonieva          ###   ########.fr       */
+/*   Updated: 2024/09/29 22:38:29 by yonieva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,16 +59,14 @@ void	parse_after_chevron(t_chevron_data *data)
 	char	*end_of_value;
 	char	*value_part;
 
-	data->current_position = data->chevron + data->token_length;
-	while (*(data->current_position) == ' ')
-		data->current_position++;
+	*(data->current_position) = data->chevron + data->token_length;
+	while (**(data->current_position) == ' ')
+		(*data->current_position)++;
 	end_of_value = ft_strchr(*(data->current_position), ' ');
 	if (end_of_value == NULL)
-		end_of_value = *(data->current_position)
-			+ ft_strlen(*(data->current_position));
-	value_part = ft_strndup(*(data->current_position), end_of_value
-			- *(data->current_position));
+		end_of_value = *(data->current_position) + ft_strlen(*(data->current_position));
+	value_part = ft_strndup(*(data->current_position), end_of_value - *(data->current_position));
 	append_chevron(data->head, data->types[data->token_index], value_part);
 	free(value_part);
-	data->current_position = end_of_value;
+	*(data->current_position) = end_of_value;
 }
